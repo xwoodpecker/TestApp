9398f05ad9b58acc529d7c4aa69096aee6ba6346
Maverick Studer <maverick.studer95@gmail.com>
2020-12-11 18:52:55 +0100

    zzzzzz

--- src/main/java/com/javainuse/UserRestController.java
+++ src/main/java/com/javainuse/UserRestController.java
@@ -3,17 +3,14 @@ package com.javainuse;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
-@Tag(name = "User management", description = "Endpoint to manage users")
+//@Tag(name = "User management", description = "Endpoint to manage users")
 public class UserRestController {
     private UserRepository userRepository;
 
@@ -21,14 +18,21 @@ public class UserRestController {
         this.userRepository = userRepository;
     }
 
+    @Operation(summary = "Get all users")
+    @GetMapping(path = "/")
+    public ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
 
     @Operation(summary = "Get all users")
-    @GetMapping("/")
-    public ResponseEntity<List<User>> getUsers(){
-        List<User> list = new ArrayList<>();
-        User user = new User();
-        user.setUserName("Karl");
-        list.add(user);
-        return new ResponseEntity<>(list, HttpStatus.OK);
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        ResponseEntity<User> response;
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+
 }
