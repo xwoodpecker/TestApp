7dcbf554c0de81c8ce684c26636423a22d53c47e
Maverick Studer <maverick.studer95@gmail.com>
2020-12-18 20:16:28 +0100

    zzzzzzsagdagsdgd

---
+++ src/main/java/com/javainuse/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package com.javainuse;
+
+import htw.smartcity.aggregator.base.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * The type Web security config.
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    private final String INITIAL_ADMIN_PASSWORD = ConfigProperties.INITIAL_ADMIN_PASSWORD;
+
+    private final UserDetailsService userDetailsService;
+
+    private final UserRepository userRepository;
+
+
+    public WebSecurityConfig(UserDetailsService userDetailsService, UserRepository userRepository) {
+        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Password encoder password encoder.
+     *
+     * @return the password encoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        try {
+            userDetailsService.loadUserByUsername("admin");
+        } catch (UsernameNotFoundException e) {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder().encode(INITIAL_ADMIN_PASSWORD));
+            admin.setEnabled(true);
+            Roles adminRole = new Roles();
+            Roles userRole = new Roles();
+            adminRole.setUser(admin);
+            userRole.setUser(admin);
+            adminRole.setRole("ADMIN");
+            userRole.setRole("USER");
+            admin.getRoles().add(adminRole);
+            admin.getRoles().add(userRole);
+            userRepository.save(admin);
+        }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.httpBasic()
+                .and().authorizeRequests().antMatchers("/swagger**").permitAll()
+                .and().authorizeRequests().anyRequest().authenticated()
+                .and().csrf().disable();
+    }
+}
